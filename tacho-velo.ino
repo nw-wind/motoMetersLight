c592000925aa732b357e4ec58be1391264b49ff8
Sergei Keler <sergei@keler.net>
2017-01-02 19:32:11 +0300

    fram works to save odo & mh, rpm & velo fixed and tested.

--- tacho-velo.ino
+++ tacho-velo.ino
@@ -3,16 +3,24 @@
 #include <RTCx.h>
 #include "FM24I2C.h"
 
-/* 
- * Тахометр 
- * Спидометр
- * Одометр с сохранением в FRAM
- * Одометр (с сохранением) со сбросом
- * Средняя скорость
- * Средняя скорость с учётом остановок
- * Напряжение
- * Время круга
- */
+/*
+   Тахометр
+   Спидометр
+   Одометр с сохранением в FRAM
+   Одометр (с сохранением) со сбросом
+   Средняя скорость
+   Средняя скорость с учётом остановок
+   Напряжение
+   Время круга
+*/
+
+// Saved data
+typedef struct FramSaved {
+  unsigned long   odoM;   // В метрах
+  unsigned long   motoS;  // Секунды от первого старта
+  unsigned long   odoM1;  // Одометр 1
+  unsigned long   odoM2;  // Одометр 2
+} FramSaved;
 
 // Smart Delay class
 class smartDelay {
@@ -27,14 +35,14 @@ class smartDelay {
     unsigned long Set(unsigned long tick);
 };
 smartDelay::smartDelay(unsigned long tick) {
-  smMicros=tick;
+  smMicros = tick;
 }
 smartDelay::~smartDelay() {}
 int smartDelay::Now() {
-  unsigned long mcs=micros();
+  unsigned long mcs = micros();
   // Важно помнить про переполнение целого!
-  if (mcs-smLast >= smMicros) {
-    smLast=mcs;
+  if (mcs - smLast >= smMicros) {
+    smLast = mcs;
     return true;
   } else {
     return false;
@@ -44,14 +52,14 @@ unsigned long smartDelay::Get() {
   return smMicros;
 }
 unsigned long smartDelay::Set(unsigned long tick) {
-  unsigned long old=smMicros;
-  smMicros=tick;
+  unsigned long old = smMicros;
+  smMicros = tick;
   return old;
 }
 // end of Smart Delay class definition
 
 static char buf[128];
-const unsigned long maxLong=4294967295;
+const unsigned long maxLong = 4294967295;
 
 // Тахометр
 // Количество тиков для усреднения
@@ -59,86 +67,90 @@ const unsigned long maxLong=4294967295;
 static unsigned long rpm;                             // Обороты в минуту для отображения
 static unsigned long rpmDist;
 //static unsigned long rpmTicks[RPMTICKS];              // Отсчёты для усреднения
-volatile int rpmTick=0;
-volatile unsigned long rpmMicros=0;
+volatile int rpmTick = 0;
+volatile unsigned long rpmMicros = 0;
 // minTick < Интервал между прерываниями < maxTick
 // Меньше: тик пропускается (многоискровое зажигание)
 // Больше: rpm=0;
-static const unsigned long rpmMin=1000000*60UL/30000;  // Минимальный тик
-static const unsigned long rpmMax=1000000*60UL/100;    // Максимальный тик
-static const unsigned int rpmDivide=1;      // Умножить для коррекции 
-static const unsigned int rpmMultiply=1;    // Разделить для коррекции
-const unsigned long rpmDelay=1*100*1000UL; // 0.1 sec
-const int rpmPin=3; // pin 3
+static const unsigned long rpmMin = 1000000 * 60UL / 30000; // Минимальный тик
+static const unsigned long rpmMax = 1000000 * 60UL / 100; // Максимальный тик
+static const unsigned int rpmDivide = 1;    // Умножить для коррекции
+static const unsigned int rpmMultiply = 1;  // Разделить для коррекции
+const unsigned long rpmDelay = 2 * 100 * 1000UL; // 0.2 sec
+const int rpmPin = 3; // pin 3
 smartDelay rpmUpdate(rpmDelay);
 
 //  Спидометр
-volatile unsigned long velo;
+static unsigned long velo;
 volatile unsigned long veloTick;
-const unsigned long veloMin= 1* 100000UL;
-const unsigned long veloMax= 1*1000000UL;
-const float veloDiameter=21;
-const float veloLength=veloDiameter*25.4*3.1416f;
-const unsigned long veloDelay=1*100*1000UL; // 0.1 sec
-const int veloPin=2; // pin 2
+volatile unsigned long veloMicros;
+static unsigned long veloDist;
+const unsigned long veloMin = 1 * 100000UL;
+const unsigned long veloMax = 1 * 1000000UL;
+const float veloDiameter = 21;
+const float veloLength = veloDiameter * 25.4 * 3.1416f;
+const unsigned long veloDelay = 2 * 100 * 1000UL; // 0.1 sec
+const int veloPin = 2; // pin 2
 smartDelay veloUpdate(veloDelay);
 
 // Дисплей
-static const unsigned long dispDelay=1*1000000UL; // Microseconds
+static const unsigned long dispDelay = 1 * 1000000UL; // Microseconds
 smartDelay dispUpdate(dispDelay);
-static byte displayColon=0;
-const int displayAddress=0x27;
+static byte displayColon = 0;
+const int displayAddress = 0x27;
 LiquidCrystal_PCF8574 lcd(displayAddress);
-static byte displayOK=0;
+static byte displayOK = 0;
 
 // Одометр
-static unsigned long odo;
+static unsigned long odoM; // м
+static unsigned long odoCm; // см
+const unsigned long odoDelay = 1 * 1000UL * 1000UL;
+smartDelay odoUpdate(odoDelay);
+const unsigned long odoDelayKm = 10000UL; // 100m by cm
 
 // Температура
 static int temp;
-static byte tempOK=0;
+static byte tempOK = 0;
 
 // Моточасы
 static unsigned int motoHours;
 static unsigned int motoMinutes;
 static unsigned long motoLast;
+static unsigned long motoSec;
 static unsigned long motoMillis;
-const unsigned long motoDelay=1*60*1000000L;
+const unsigned long motoDelay = 1 * 60 * 1000000L;
+const unsigned long motoDelaySec = 1 * 1 * 1000000L;
 smartDelay motoUpdate(motoDelay);
+smartDelay motoUpdateSec(motoDelaySec);
 
 // FRAM
-const int framAddress=0x57;
+const int framAddress = 0x57;
 FM24I2C fram(framAddress);
-static byte framOK=0;
+static byte framOK = 0;
+FramSaved saved;
+const unsigned long framDelay = 10 * 1000L * 1000L; // частота записи в fram
+const int framOffset = 0x00;
+smartDelay framUpdate(framDelay);
 
 // RTC
-static byte rtcOK=0;
+static byte rtcOK = 0;
+
+// keep alive led
+smartDelay ledUpdate(1000000UL);
+static byte ledState = HIGH;
 
 // Прерывания
 
 void intRpm() {
-  //unsigned long mcs=micros();
-  //sprintf(buf,"Rpm tick: %ld ",mcs-rpmTicks[0]);
-  //Serial.println(buf);
-  //if ((mcs-rpmDist)>rpmMin) {
-  //  rpmDist=mcs;
-    //Serial.println("Good rpm tick");
-  //}
-  
   /// TODO если не слишком быстро...
   rpmTick++;
-  rpmMicros=micros();
+  rpmMicros = micros();
 }
 
 void intVelo() {
-  unsigned long mcs=micros();
-  //sprintf(buf,"Velo tick: %ld ",mcs-veloTick);
-  //Serial.println(buf);
-  if ((mcs-veloTick)>veloMin) {
-    veloTick=mcs;
-    //Serial.println("Good velo tick");
-    odo+=(unsigned long)(veloLength);
-  }
+  veloTick++;
+  veloMicros = micros();
+  odoCm += (unsigned long)(veloLength / 10.0f ); // /100
 }
 
 // printAt()
@@ -149,9 +161,9 @@ void printAt(int c, int r, const char *s, ...) {
   va_list ap;
   va_start(ap, s);
   if (displayOK) {
-    lcd.setCursor(c,r);
+    lcd.setCursor(c, r);
     vsnprintf(buf, sizeof(buf), s, ap);
-    lcd.print(buf); 
+    lcd.print(buf);
   }
   va_end(ap);
 }
@@ -159,55 +171,67 @@ void printAt(int c, int r, const char *s, ...) {
 void setup() {
   Wire.begin();
   Serial.begin(9600);
-  
-  pinMode(veloPin,INPUT_PULLUP);
-  pinMode(rpmPin,INPUT_PULLUP);
+
+  pinMode(veloPin, INPUT_PULLUP);
+  pinMode(rpmPin, INPUT_PULLUP);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
   //digitalWrite(veloPin,HIGH); // pull up
   //digitalWrite(rpmPin,HIGH);   // pull down
   Serial.begin(9600);
-  attachInterrupt(digitalPinToInterrupt(rpmPin),intRpm,RISING);
-  attachInterrupt(digitalPinToInterrupt(veloPin),intVelo,RISING);
+  attachInterrupt(digitalPinToInterrupt(rpmPin), intRpm, RISING);
+  attachInterrupt(digitalPinToInterrupt(veloPin), intVelo, RISING);
 
   Wire.beginTransmission(displayAddress);
   if (Wire.endTransmission(true) == 0) {
-    displayOK=true;
+    displayOK = true;
   } else {
-    displayOK=false;
+    displayOK = false;
   }
 
   if (displayOK) {
     lcd.begin(16, 2);
     lcd.setBacklight(200);
     lcd.noCursor();
-    lcd.home(); 
+    lcd.home();
     lcd.clear();
-    printAt(0,0,"%16s","DISPLAY OK");
+    printAt(0, 0, "%16s", "DISPLAY OK");
   }
   delay(2000);
 
   // Добавить в класс!
   Wire.beginTransmission(framAddress);
   if (Wire.endTransmission(true) == 0) {
-    framOK=true;
-    printAt(0,0,"%16s","FRAM OK");
+    framOK = true;
+    printAt(0, 0, "%16s", "FRAM OK");
+    fram.unpack(framOffset, &saved, sizeof(saved));
+    odoM = saved.odoM;
+    motoSec = saved.motoS;
+    motoHours = motoSec / 60 / 60;
+    motoMinutes = (motoSec / 60) % 60;
+    //printAt(0, 1, "%ld %ld", odoM, motoSec);
   } else {
-    framOK=false;
-    printAt(0,0,"%16s","FRAM ERROR");
+    framOK = false;
+    printAt(0, 0, "%16s", "FRAM ERROR");
   }
- 
+
   delay(2000);
 
   uint8_t addressList[] = {RTCx::MCP7941xAddress, RTCx::DS1307Address};
   if (rtc.autoprobe(addressList, sizeof(addressList))) {
     rtc.enableBatteryBackup();
     rtc.startClock();
-    rtcOK=true;
-    printAt(0,0,"%16s","RTC OK");
+    rtcOK = true;
+    printAt(0, 0, "%16s", "RTC OK");
   } else {
-    printAt(0,0,"%16s","RTC ERROR");
+    printAt(0, 0, "%16s", "RTC ERROR");
   }
+
+  //if (!rtc.setSQW(RTCx::freq4096Hz)) Serial.println("Fail to set SW");
+
   delay(2000);
   if (displayOK) lcd.clear();
+
   Serial.println("Ready");
 }
 
@@ -218,98 +242,112 @@ void displayClock() {
     // Читать часы
     rtc.readClock(tm);
     // Вывести их
-    sprintf(buf,"%02d%s%02d",tm.tm_hour,(displayColon?":":" "),tm.tm_min,tm.tm_sec);
+    sprintf(buf, "%02d%s%02d", tm.tm_hour, (displayColon ? ":" : " "), tm.tm_min, tm.tm_sec);
     lcd.setCursor(11, 1);
     lcd.print(buf);
-    displayColon=(displayColon+1)%2;
+    displayColon = (displayColon + 1) % 2;
   }
 }
 
 void displaySpeed() {
-  printAt(0,0,"%3d",velo);
+  printAt(0, 0, "%3d", velo);
 }
 
 void displayRpm() {
-  printAt(4,0,"%5d",rpm);
+  printAt(4, 0, "%5ld", rpm);
 }
 
 void displayOdo() {
   if (framOK) {
-    printAt(10,0,"%6ld",odo/1000000L);
+    printAt(10, 0, "%6ld", odoM / 1000UL);
   }
 }
 
 void displayTemp() {
-  int t=(temp>0?temp:0);
+  int t = (temp > 0 ? temp : 0);
   if (tempOK) {
-    printAt(0,1,"%3d",t);
+    printAt(0, 1, "%3d", t);
   }
 }
 
 void displayMH() {
-  printAt(4,1,"%3u:%02u",motoHours,motoMinutes);
+  printAt(4, 1, "%3u:%02u", motoHours, motoMinutes);
 }
 
-int CalcRPM() {
-  //unsigned long mcs=micros();
-     // вычисляем обороты
-    /*
-    unsigned long rd=rpmDist;
-    if ((mcs-rd)>rpmMin && (mcs-rd)<rpmMax) {
-      rpmTicks[rpmTick]=1000000UL*60/(mcs-rd);
-      sprintf(buf,"Good rpm tick: %ld %ld",mcs-rpmTicks[rpmTick],rpm);
-      Serial.println(buf);
-    } 
-    if ((mcs-rd)>rpmMax) { 
-      rpmTicks[rpmTick]=0;
-    }
-    rpm=0;
-    for (int i=0; i<RPMTICKS; i++) {
-      rpm+=rpmTicks[i];
-    }
-    rpm/=RPMTICKS;
-    rpmTick=(rpmTick+1)%RPMTICKS;
-    */
-    
-    /// TODO:
-    // Добавить минимальное и максимальное.
-    if (rpmTick > 0) {
-      rpm=1000000L*60L/((rpmMicros-rpmDist)/rpmTick);
-      rpmTick=0;
-      rpmDist=rpmMicros;
-    } else {
-      rpm=0;
-    } 
+unsigned long CalcRPM() {
+  /// TODO:
+  // Добавить минимальное и максимальное.
+  if (rpmTick > 0) {
+    rpm = 1000000L * 60L / ((rpmMicros - rpmDist) / rpmTick);
+    //rpm /= 100; /////
+    rpmTick = 0;
+    rpmDist = rpmMicros;
+  } else {
+    rpm = 0;
+  }
+  if (rpm > 30000) rpm = 0;
+  //Serial.println(rpm);
   return rpm;
 }
 
-void loop() {
-  unsigned long mcs=micros();
-  
-  // Вычислить обороты
-  if (rpmUpdate.Now()) rpm=CalcRPM();
-  // Вычислить скорость
-  if (veloUpdate.Now()) {
-    unsigned long vt=veloTick;
-    if ((mcs-vt)>veloMin && (mcs-vt)<veloMax) {
-      velo=(veloLength/1000.0f/1000.0f)/((mcs-vt)/1000000.0f/3600.0f);
-      //sprintf(buf,"Good velo tick: %ld %ld %d",mcs-veloTick,velo,((mcs-veloTick)>veloMax));
-      //Serial.println(buf);
-    } 
-    if ((mcs-vt)>veloMax) velo=0;
+unsigned long  CalcSpeed() {
+  if (veloTick > 0) {
+    velo = veloLength / ((float)(veloMicros - veloDist) / veloTick / 3600.f);
+    //velo /= 100;
+    //sprintf(buf, "Good velo tick: %ld %ld %ld", velo, veloMicros - veloDist, veloTick);
+    //Serial.println(buf);
+    veloTick = 0;
+    veloDist = veloMicros;
+  } else {
+    velo = 0;
   }
+  if (velo > 400) velo = 0;
+  return velo;
+}
+
+void loop() {
+  unsigned long mcs = micros();
+  unsigned long mls = millis();
+
+  if (ledUpdate.Now()) {
+    ledState = (ledState == HIGH ? LOW : HIGH);
+    digitalWrite(13, ledState);
+  }
+  // Вычислить обороты
+  if (rpmUpdate.Now()) rpm = CalcRPM();
+  // Вычислить скорость
+  if (veloUpdate.Now()) velo = CalcSpeed();
   // Моточасы
-  if (motoUpdate.Now()) {
+  if (motoUpdateSec.Now()) {
     if (rpm) {
       // Часы идут
-      motoMillis+=(mcs-motoLast)/1000L;
-      motoHours=motoMillis/1000/60/60;
-      motoMinutes=(motoMillis/1000/60)%60;
+      //Serial.println("MH ticks");
+      motoMillis += mls - motoLast;
     } else {
       // Часы стоят
-      
+      //Serial.println("MH sleeps");
     }
-    motoLast=mcs;
+    motoLast = mls;
+  }
+  if (motoUpdate.Now()) {
+    Serial.println("MH updates");
+    motoSec += motoMillis / 1000UL;
+    motoHours = motoSec / 60 / 60;
+    motoMinutes = (motoSec / 60) % 60;
+    motoMillis = 0;
+  }
+  if (odoUpdate.Now()) {
+    if (odoCm > odoDelayKm) {
+      // пора обновить одометр в метрах
+      odoM += odoCm / 100;
+      odoCm = 0;
+    }
+  }
+  if (framUpdate.Now() && framOK) {
+    saved.odoM = odoM;
+    saved.motoS = motoSec;
+    fram.pack(framOffset, &saved, sizeof(saved));
+    //Serial.println("Fram saved");
   }
   // Показать на дисплее
   if (dispUpdate.Now()) {
